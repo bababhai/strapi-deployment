0ea31286582eef5a318f508df2e0705b356c9651
DrCoder <shakil.7878@gmail.com>
2024-10-29 23:47:01 +0530

    updated server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 1337;
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+// @ts-ignore
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server listening on port ${PORT}`);
 });
